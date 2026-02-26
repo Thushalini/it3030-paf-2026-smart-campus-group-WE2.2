372f03bdccd320f643957c5611dfb82581982eca
Thushalini Peranantham <it23231450@my.sliit.lk>
2026-02-26 16:34:00 +0530

    backend successfully created

---
+++ backend/smart_campus-core/src/main/java/com/sliit/smart_campus_core/SmartCampusCoreApplication.java
@@ -0,0 +1,13 @@
+package com.sliit.smart_campus_core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartCampusCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartCampusCoreApplication.class, args);
+	}
+
+}
---
+++ backend/smart_campus-core/src/test/java/com/sliit/smart_campus_core/SmartCampusCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sliit.smart_campus_core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartCampusCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
